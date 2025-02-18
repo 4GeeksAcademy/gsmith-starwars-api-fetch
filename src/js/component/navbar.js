d266b21c335cad85a8e1e53dc412cb3288e76950
1122gs <GORDON.SMITHJR@hotmail.com>
2025-02-18 14:04:14 +0000

    almost done

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -11,40 +11,46 @@ export const Navbar = () => {
 
   return (
     <nav className="navbar navbar-light bg-light px-3">
-      <Link className="navbar-brand" to="/">Star Wars</Link>
+      <Link className="navbar-brand" to="/">
+        Star Wars
+      </Link>
 
       <div className="dropdown">
-        <button 
-          className="btn btn-primary dropdown-toggle" 
+        <button
+          className="btn btn-primary dropdown-toggle"
           onClick={() => setShowDropdown(!showDropdown)}
         >
-          Favorites <span className="badge bg-danger">{store.favorites.length}</span>
+          Favorites{" "}
+          <span className="badge bg-danger">{store.favorites.length}</span>
         </button>
 
         {showDropdown && (
-          <div className="dropdown-menu dropdown-menu-end show" style={{ width: "250px" }}>
+          <div
+            className="dropdown-menu dropdown-menu-center show"
+            style={{ width: "50px" }}
+          >
             {store.favorites.length === 0 ? (
               <span className="dropdown-item">No favorites added</span>
             ) : (
               <>
                 {store.favorites.map((fav, index) => (
-                  <div key={index} className="dropdown-item d-flex justify-content-between align-items-center">
+                  <div
+                    key={index}
+                    className="dropdown-item d-flex justify-content-between align-items-center"
+                  >
                     <span>{fav.properties.name}</span>
-                    <button 
+                    <button
                       className="btn btn-sm btn-danger"
                       onClick={() => actions.removeFavorite(fav.uid)}
                     >
-                      <FontAwesomeIcon
-                      icon={faTrash}
-                      size="lg"/>
-                      
+                      <FontAwesomeIcon icon={faTrash} size="lg" />
                     </button>
                   </div>
                 ))}
 
                 {/* Clear All Favorites Button */}
                 <div className="dropdown-divider"></div>
-                <button 
+                <button
                   className="dropdown-item text-danger text-center"
                   onClick={actions.clearFavorites}
                 >
